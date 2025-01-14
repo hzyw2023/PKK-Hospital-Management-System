550397b9eacddd5f200354a3f2cb691a81d25445
clement <clementfoong5765@gmail.com>
2025-01-14 18:00:43 +0800

    update checklogin

--- check_login.php
+++ check_login.php
@@ -1,7 +1,10 @@
-<?php 
-if((isset($_SESSION["email"]) && isset($_SESSION["password"]))){
-	$myemail = $_SESSION['email'];
-}else {
-	header("location:login.php");
+<?php
+session_start();
+if(!(isset($_SESSION["email"]) && isset($_SESSION["password"]) && isset($_SESSION["user"]))){
+    // Store current URL for redirect after login
+    $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI'];
+    header("location:login.php");
+    exit(); // Add exit after redirect
 }
+$myemail = $_SESSION['email'];
 ?>
